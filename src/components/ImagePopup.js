780abb960ee5741f2c6d09de2b0b46ed899b473a
yndx_geometrichno <klementevart@yandex.ru>
2023-09-19 17:29:24 +0500

    refactor: code and files

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function ImagePopup(props) {
-  console.log(props);
   return (
     <div
       className={
--- src/utils/utils.js
+++
@@ -1,4 +0,0 @@
-export const cardsContainerSelector = '.cards';
-export const popupProfileSelector = '.popup_type_profile';
-export const popupCardSelector = '.popup_type_card';
-export const popupChangeAvatarSelector = '.popup_type_profile-pic-update'
